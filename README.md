7055a95b219dab0cbb8ec38dd09af698950e2f70
ldotmithu <ldotmithurshan222@gmail.com>
2024-09-01 03:23:47 +0530

    evel

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 
 git config --global user.email "ldotmithurshan222@gmail.com"
 git config --global user.name "ldotmithu"
+
+
+import dagshub
+dagshub.init(repo_owner='ldotmithu', repo_name='End_To_End_ML_MLFLOW', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
+
+
+  https://dagshub.com/ldotmithu/End_To_End_ML_MLFLOW.mlflow
+
+  ec98f4a9bd5ed641197839391e079d81a56e4f9a
+
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/ldotmithu/End_To_End_ML_MLFLOW.mlflow
+
+export MLFLOW_TRACKING_USERNAME=ldotmithu 
+
+export MLFLOW_TRACKING_PASSWORD=ec98f4a9bd5ed641197839391e079d81a56e4f9a
+
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -4,7 +4,7 @@ from mlProject.entity.config_entity import (DataIngestionConfig,
                                             DataValidationConfig,
                                             DataTransformationConfig,
                                             ModelTrainerConfig,
-                                            )
+                                            ModelEvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -86,6 +86,27 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/ldotmithu/End_To_End_ML_MLFLOW.mlflow",
+           
+        )
+
+        return model_evaluation_config
     
 
 
